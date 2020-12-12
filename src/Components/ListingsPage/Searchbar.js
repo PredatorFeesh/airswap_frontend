c8c75bbe173eb1b2b724f1a549e9a3b23e21d3f9
Jason Azayev <azayevjason@gmail.com>
2020-12-12 01:41:22 -0500

    Listing works and Searchbar works

--- src/Components/ListingsPage/Searchbar.js
+++ src/Components/ListingsPage/Searchbar.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 
+import { cities } from '../../Utils/requests';
+
 export class Searchbar extends React.Component{
     
     constructor(props) {
         super(props);
         this.state = {
-          
-          cities: [
-            {name:"London"}, {name: "Moscow"}
-            ],
-          
+          cities: [{Name: "Loading..."}],
         };
+    }
+
+    componentDidMount() {
+        this.getCities();
+    }
+
+    getCities() {
+        cities().then(data => {
+            if (data == false) {
+                this.setState({cities: []});
+                return;
+            }
+            const cities = [{Name: "Followed Cities"}].concat(data.Cities);
+            this.setState({cities});
+        });
+    }
+
+    searchCity = (event) => {
+        this.props.searchCity(event.target.value);
     }
     
 
     render(){
-    
         let optionsArr=[]
         for( let i=0; i<this.state.cities.length; i++){
-            optionsArr.push(<option  key={i}>{this.state.cities[i].name}</option>)
+            optionsArr.push(<option  key={i}>{this.state.cities[i].Name}</option>);
         }
 
       return (
@@ -32,7 +48,7 @@ export class Searchbar extends React.Component{
                 <Form.Group controlId="exampleForm.ControlSelect1" placeholder="Large text">
       
                 <h3>Search for your next destination!</h3>
-                    <Form.Control as="select" value={this.state.citySelected}  >
+                    <Form.Control as="select" value={this.state.citySelected} onChange={this.searchCity} >
                         {optionsArr}
                     </Form.Control>
                 </Form.Group>
--- src/Pages/ListingsPage.js
+++ src/Pages/ListingsPage.js
@@ -2,15 +2,56 @@ import React from 'react';
 import { ListingsContainer } from '../Components/ListingsPage/ListingsContainer.js';
 import { Searchbar } from '../Components/ListingsPage/Searchbar.js';
 
+import { listings } from "../Utils/requests";
+
 export class ListingsPage extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      listings: [],
+    };
+  }
+
+    componentDidMount() {
+      this.updateListings();
+    }
+
+    updateListings = () => {
+        listings().then(data => {
+            if (data == false) {
+                // If we failed the request
+                this.setState({listings: []});
+                return;
+            }
+            this.setState({listings: data});
+        });
+    }
+
+    searchCity = (cityName) => {
+      if(cityName == "Followed Cities") {
+        this.updateListings();
+      } else {
+        listings(cityName).then(data => {
+          if (data == false) {
+            return;
+          }
+          this.setState({listings: data.Listings});
+        });
+      }
+    }
       
     render(){
     
       return (
         <div >
             <h1 style={{textAlign: "center"}}>Listings Page</h1>
-            <Searchbar/>
-            <ListingsContainer/>
+            <Searchbar
+              searchCity={this.searchCity}
+            />
+            <ListingsContainer
+              listings={this.state.listings}
+            />
         </div>
         );
       };
