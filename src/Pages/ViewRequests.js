2731ed4f300a77d66b15358bab07eff6670c7772
Jason Azayev <azayevjason@gmail.com>
2020-12-14 13:28:59 -0500

    rendering the lists

--- src/Pages/ViewRequests.js
+++ src/Pages/ViewRequests.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { ListingsCard } from '../Components/ListingsPage/ListingsCard.js'; 
+
 import {sentRequests} from '../Utils/requests';
 
 export class ViewRequests extends React.Component{
@@ -19,7 +21,7 @@ export class ViewRequests extends React.Component{
             UserID: 9
         */
         this.state = {
-            requests: []
+            listings: []
         };
     }
 
@@ -27,16 +29,40 @@ export class ViewRequests extends React.Component{
         sentRequests().then(data => {
             if (data != false) {
                 const list = data["Requested"];
-                this.setState({requests: list});
+                this.setState({listings: list});
             }
         });
     }
 
     render(){
+
+        let listingsArr=[]
+        for(let i=0; i<this.state.listings.length; i++){
+            listingsArr.push(
+                    <ListingsCard address={this.state.listings[i].Listing.Address} 
+                                  city={this.state.listings[i].Listing.City}
+                                  date={this.state.listings[i].Listing.Date}
+                                  description={this.state.listings[i].Listing.Description}
+                                  img={this.state.listings[i].Listing.Image}
+                                  listingId={this.state.listings[i].Listing.ListingID}
+                                  isListed={this.state.listings[i].Listing.is_listed}
+                                  ownerDescription={this.state.listings[i].UserDescription}
+                                  email={this.state.listings[i].Email}
+                                  lastName={this.state.listings[i]["First Name"]}
+                                  ownerImg={this.state.listings[i].Image}      
+                                  phoneNumber={this.state.listings[i]["Phone Number"]}
+                                  ownerId={this.state.listings[i].UserID}  
+                                  key={`list-${i}`}                        
+                                  />
+                );
+        }
     
       return (
         <div className="RequestsPage">
             <h1>You have requested to swap with</h1>
+
+            {listingsArr}
+
         </div>
         );
       };
